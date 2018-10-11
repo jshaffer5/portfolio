97558a96b0c2cf5e71ade91a708685bd1be6da75
js <js@iMac.local>
2018-10-11 01:42:36 -0700

    basic portfolio skeleton

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.port;
+if (port==null) {port= 3000};
+
+app.listen(port, function(){
+    console.log('listening on ', port);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/contact.html');
+})
