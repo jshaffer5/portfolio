af33b1652f3acda32e10c869d486562ae858ee5b
js <joshtwoone@gmail.com>
2019-01-29 05:14:39 -0800

    added jquery slideDown() animations for projects

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ function showMore() {
   let htmlCollection = document.getElementsByClassName("more-cards");
   let elements = Array.from(htmlCollection);
   console.log("showMore() called");
-  elements.map(element => {
-    if (element.style.display != "block") {
-      element.style.display = "block";
-      document.getElementById("show-more-projects").innerText = "Show Less";
+  // elements.map(element => {
+  //   if (element.style.display != "block") {
+  //     element.style.display = "block";
+  //     document.getElementById("show-more-projects").innerText = "Show Less";
+  //   } else {
+  //     element.style.display = "none";
+  //     document.getElementById("show-more-projects").innerText = "Show More";
+  //   }
+  // });
+
+  $( document ).ready(function() {
+    if ($(".more-cards").css('display') != "block"){
+      $(".more-cards").slideDown("slow", function() {
+          console.log("slideDown() called, display is set to: ", $(this).css('display'));
+          // $(this).attr("style", "display:block");
+          $("#show-more-projects").text("Show Less");
+      });
     } else {
-      element.style.display = "none";
-      document.getElementById("show-more-projects").innerText = "Show More";
+      $(".more-cards").slideUp("fast", function() {
+        console.log("slideUp() called", $(this).css('display'));
+        // $(this).attr("style", "display:none")
+        $("#show-more-projects").text("Show More");
+      });
     }
+  
   });
 }
+
+
